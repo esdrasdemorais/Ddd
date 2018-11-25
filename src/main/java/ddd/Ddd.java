fd4412515b3b07b157a60e1d135da2b29eac8665
Esdras de Morais da Silva <esdrasdemorais@gmail.com>
2018-11-25 15:45:07 -0200

    Refactored Mongo Connection String

---
+++ src/main/java/ddd/Ddd.java
@@ -0,0 +1,7 @@
+package ddd;
+
+public class Ddd {
+    public static void main(String[] args) {
+    	System.out.println("AMOR="+System.getProperty("mongo.uri"));
+    }
+}
---
+++ src/test/java/ddd/User.java
@@ -0,0 +1,22 @@
+package ddd;
+
+public class User {
+	private Long id;
+	private String name;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
