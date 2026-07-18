e2349ba9f17cf51650dd3ab2fc59342f2d0d7ee4
Esdras de Morais da Silva <4275356+esdrasdemorais@users.noreply.github.com>
2026-07-18 20:27:45 -0300

    Mocked with Mockito Unit Test

--- src/test/java/ddd/UserRepository.java
+++ src/test/java/ddd/UserRepository.java
@@ -1,12 +1,17 @@
 package ddd;
 
+import static org.junit.Assert.*;
+
+import com.esdrasmorais.ddd.repository.MongoContext;
 import com.esdrasmorais.ddd.repository.MongoRepository;
 import com.esdrasmorais.ddd.repository.interfaces.IClient;
 import com.esdrasmorais.ddd.repository.interfaces.IContext;
 import com.esdrasmorais.ddd.repository.interfaces.IDb;
 
 public class UserRepository extends MongoRepository<User> {
-	public UserRepository(IContext context, IClient client, IDb db) {
-		super(context, client, db);
-	}
+    private int bulkMaxNumber = 1000;
+    
+    public UserRepository(IContext context, IClient client, IDb db) {
+	super(context, client, db);
+    }
 }
